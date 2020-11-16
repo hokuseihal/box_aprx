796ddb0a65f1e2d2e8945c4dbcee0f459d03c483
hokuseihal <hokuseihal@gmail.com>
2020-11-16 19:59:37 +0900

    fix path

--- model/FC_Resnet.py
+++ model/FC_Resnet.py
@@ -2,63 +2,77 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 class BasicBlock(nn.Module):
-    def __init__(self, feature):
+    def __init__(self, feature,batchnorm=False):
         super(BasicBlock, self).__init__()
         self.fc1 = nn.Linear(feature, feature)
         self.fc2 = nn.Linear(feature, feature)
+        self.bn1=nn.BatchNorm1d(feature)
+        self.bn2=nn.BatchNorm1d(feature)
+        self.batchnorm=batchnorm
 
     def forward(self, x):
         _x = x
         x = self.fc1(x)
+        if self.batchnorm:
+            x=self.bn1(x)
         x = F.relu(x)
         x = self.fc2(x)
+        if self.batchnorm:
+            x=self.bn2(x)
+        x = F.relu(x)
         x = _x + x
         x = F.relu(x)
         return x
 
 
 class BottleNeck(nn.Module):
-    def __init__(self, feature, fn_act=nn.ReLU()):
+    def __init__(self, feature, fn_act=nn.ReLU(),batchnorm=False):
         super(BottleNeck, self).__init__()
         self.fcs = nn.ModuleList([nn.Linear(feature, feature) for _ in range(3)])
+        self.bns = nn.ModuleList([nn.BatchNorm1d(feature) for _ in range(3)])
         self.fn_act = fn_act
-
+        self.batchnorm=batchnorm
     def forward(self, x):
         _x = x
-        x = self.fn_act(self.fcs[0](x))
-        x = self.fn_act(self.fcs[1](x))
+        x = self.fcs[0](x)
+        if self.batchnorm:
+            x=self.bns[0](x)
+        x=self.fn_act(x)
+        x = self.fcs[1](x)
+        if self.batchnorm:
+            x=self.bns[1](x)
+        x=self.fn_act(x)
         x = self.fcs[2](x)
+        if self.batchnorm:
+            x=self.bns[2](x)
         return self.fn_act(x + _x)
 
 
 class FC_ResNet(nn.Module):
-    def __init__(self, block, layers=(1, 2, 3, 4), features=(128, 128, 256, 512), fn_activate=nn.ReLU(),flow_loss=False):
+    def __init__(self, block, layers=(1, 2, 3, 4), features=(128, 128, 256, 512), fn_activate=nn.ReLU(),flow_loss=False,batchnorm=False):
         super(FC_ResNet, self).__init__()
         self.infc = nn.Linear(116, features[0])
         self.fcs = nn.ModuleList(
             [nn.Sequential(
-                *[block(features[idx]) for _ in range(l)]
+                *[block(features[idx],batchnorm) for _ in range(l)]
             ) for idx, l in enumerate(layers)]
         )
         self.changer=nn.ModuleList([nn.Linear(in_feature,out_feature) for in_feature,out_feature in zip(features[:-1],features[1:])])
-        self.outfc = nn.ModuleList([nn.Linear(f, 20) for f in features])
+        self.outfc = nn.Linear(features[-1], 20)
         self.activate = fn_activate
         self.flow_loss=flow_loss
     def forward(self, x):
         x = self.activate(self.infc(x))
-        ret = []
-        for idx,(fc,out) in enumerate(zip(self.fcs,self.outfc)):
+        for idx,fc in enumerate(self.fcs):
             x = fc(x)
-            ret.append(torch.sigmoid(out(x)))
-            if idx<3:x=self.changer[idx](x)
-        if self.flow_loss:
-            return ret
-        else:
-            return ret[-1]
+            if idx<3:
+                x=self.changer[idx](x)
+        x=self.outfc(x)
+        return [x]
 
 
 def fc_resnet18():
-    return FC_ResNet(layers=[2, 2, 2, 2], block=BasicBlock)
+    return FC_ResNet(layers=[2, 2, 2, 2], block=BasicBlock,batchnorm=True)
 
 
 def fc_resnet34():
--- train.py
+++ train.py
@@ -10,13 +10,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from core import addvalue, save
-from model.FC_Resnet import fc_resnet18
+from model.FC_Resnet import fc_resnet18,fc_resnet34,fc_resnet50,fc_resnet101,fc_resnet152
 
 
 class InputParam(nn.Module):
     def __init__(self, data):
         super(InputParam, self).__init__()
-        self.data = data
+        self.optimizable=np.zeros(56,dtype=np.bool)
+        self.optimizable[3:14]=True
+        self.data=torch.stack([nn.Parameter(torch.rand(1),requires_grad=self.optimizable[i]) for i in range(56)])
 
 
 class Dataset(torch.utils.data.Dataset):
@@ -45,7 +47,7 @@ def operate(phase):
     for idx,(data,target) in enumerate(loader):
         with torch.set_grad_enabled(phase=='train'):
             all_output=model(data.to(device))
-            loss=sum([lossf(output,target.to(device)) for output in all_output])
+            loss=sum([lossf(output,target.to(device)) for output in all_output])/len(all_output)
             if phase=='train':
                 loss.backward()
                 optimizer.step()
@@ -54,13 +56,14 @@ def operate(phase):
             addvalue(writer,f'loss:{phase}',loss.item(),e)
 
 def estimate():
+    def okng(output):
+        return F.relu(output-1).mean()
     model.eval()
-    # data=loaddata()
     inputparam = InputParam(data)
     data_optimizer = torch.optim.Adam(inputparam.parameters())
     for idx in range(est_epochs):
         output = model(inputparam.data)
-        loss = F.relu(output - 1).mean()
+        loss = okng(output)
         loss.backward()
         data_optimizer.step()
         data_optimizer.zero_grad()
@@ -70,30 +73,24 @@ def estimate():
 if __name__=='__main__':
     device='cuda' if torch.cuda.is_available() else 'cpu'
     parser=argparse.ArgumentParser()
-    parser.add_argument('--feature',type=int,default=128)
-    parser.add_argument('--num_layer',type=int,default=3)
-    parser.add_argument('--num_model',type=int,default=1)
     args=parser.parse_args()
-    feature=args.feature
-    num_layer=args.num_layer
-    num_model=args.num_model
-    print(f'{device=},{feature=},{num_layer=},{num_model=}')
-    model=fc_resnet18()
+    model=fc_resnet18().to(device)
     writer={}
-    batchsize=512
-    dataset=Dataset('../data/doboku/box_aprx/data3')
+    batchsize=1024
+    dataset=Dataset('J:/data3')
     traindataset,valdataset=torch.utils.data.random_split(dataset,[dsize:=int(len(dataset)*0.8),len(dataset)-dsize])
-    trainloader=torch.utils.data.DataLoader(traindataset,batch_size=batchsize,num_workers=cpu_count())
-    valloader=torch.utils.data.DataLoader(valdataset,batch_size=batchsize,num_workers=cpu_count())
+    trainloader=torch.utils.data.DataLoader(traindataset,batch_size=batchsize,num_workers=cpu_count(),shuffle=True)
+    valloader=torch.utils.data.DataLoader(valdataset,batch_size=batchsize,num_workers=cpu_count(),shuffle=True)
     optimizer=torch.optim.Adam(model.parameters())
     lossf=nn.MSELoss()
     esp=1
     epochs=1000
     est_epochs=200
-    savefolder=f'data/{num_model}_{num_layer}_{feature}'
+    savefolder=f'data/FC_152'
     os.makedirs(savefolder,exist_ok=True)
     data=torch.rand(116)
     for e in range(epochs):
-        operate('train')
-        operate('val    ')
-        save(model,savefolder,writer,f'{feature=},{num_layer=},{num_model=}')
+        # operate('train')
+        # operate('val')
+        # save(model,savefolder,writer,f'')
+        estimate()
